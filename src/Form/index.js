8df14c3940254239145ffc099a44645ccb1a11d6
Karyna Krapiunitskaya <krapinah@gmail.com>
2022-04-24 14:39:01 +0300

    prevented calculation of negative values

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import { useState } from "react";
-import render from "./RenderResult";
+import render from "./renderResult";
 
 
 const Form = () => {
@@ -25,7 +25,7 @@ const Form = () => {
             name: "CHF",
             rate: 4.33,
         },
-    ]; 
+    ];
 
     const onInputChange = ({ target }) => setInput(target.value);
     const onOutputCurrencyChange = ({ target }) => setOutputCurrency(target.value);
@@ -43,61 +43,64 @@ const Form = () => {
 
     const onFormSubmit = (event) => {
         event.preventDefault();
+        if (input < 0) {
+            return;
+        }
         calculateResult(outputCurrency);
     };
 
     return (
-        
-            <form className="form" onClick={onFormSubmit}>
-                <fieldset className="form__fieldset">
-                    <legend className="form__legend">Przelicz walutę</legend>
-                    <p>
-                        <label className="form__label">
-                            <span className="form__labelText">
-                                Wpisz kwotę w PLN*:
-                            </span>
-                            <input
-                                className="form__input"
-                                value={input}
-                                onChange={onInputChange}
-                                required
-                                autoFocus
-                                type="number"
-                                min="0.01"
-                                step="0.01"
-                            />
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            <span className="form__labelText">
-                                Wybierz docelową walutę:
-                            </span>
-                            <select
-                                name="currency"
-                                value={outputCurrency}
-                                onChange={onOutputCurrencyChange}
-                            >
-                                {currencies.map((currency) => (
-                                    <option
-                                        key={currency.name}
-                                        value={currency.name}
-                                    >{currency.name}</option>
-                                ))}
-                            </select>
-                        </label>
-                    </p>
-                </fieldset>
-                <p className="form__paragraph">Obowiązkowe pola są oznaczone gwiazdką*.</p>
-                <button 
-                className="form__button" 
+
+        <form className="form" onClick={onFormSubmit}>
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">Przelicz walutę</legend>
+                <p>
+                    <label className="form__label">
+                        <span className="form__labelText">
+                            Wpisz kwotę w PLN*:
+                        </span>
+                        <input
+                            className="form__input"
+                            value={input}
+                            onChange={onInputChange}
+                            required
+                            autoFocus
+                            type="number"
+                            min="0.01"
+                            step="0.01"
+                        />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span className="form__labelText">
+                            Wybierz docelową walutę:
+                        </span>
+                        <select
+                            name="currency"
+                            value={outputCurrency}
+                            onChange={onOutputCurrencyChange}
+                        >
+                            {currencies.map((currency) => (
+                                <option
+                                    key={currency.name}
+                                    value={currency.name}
+                                >{currency.name}</option>
+                            ))}
+                        </select>
+                    </label>
+                </p>
+            </fieldset>
+            <p className="form__paragraph">Obowiązkowe pola są oznaczone gwiazdką*.</p>
+            <button
+                className="form__button"
                 type="submit"
-                >Oblicz</button>
-                <div className="form__result">
-                    <p className="form__paragraphResult">Kwota wynosi:<strong>{result}</strong> 
-                    </p>
-                </div>
-            </form>
+            >Oblicz</button>
+            <div className="form__result">
+                <p className="form__paragraphResult">Kwota wynosi:<strong>{result}</strong>
+                </p>
+            </div>
+        </form>
     );
 };
 
